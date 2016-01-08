d96bd2ffea68b5d944a8c84f50c11f96bfb162ec
Renato Massaro <renato@hackerexperience.com>
2016-01-08 01:08:08 -0200

    Add disqus snippet

--- post-detail.php
+++ post-detail.php
@@ -1,1 +1,20 @@
 <?php echo $post->render(); ?>
+
+<div id="disqus_thread"></div>
+<script>
+    var disqus_config = function () {
+        this.page.url = <?php echo $blog->getDomain() . '/' . $post->getPost()->getPhameTitle(); ?>
+        this.page.identifier = location.protocol + '//' + <?php echo $post->getPost()->getPhameTitle(); ?>
+    };
+    (function() {  // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+
+        s.src = '//renatomassaro.disqus.com/embed.js';
+
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+<script id="dsq-count-scr" src="//renatomassaro.disqus.com/count.js" async></script>
+
