f6b91de59edba0f65ad6c88cb225a8bd7d97789c
Renato Massaro <renato@hackerexperience.com>
2016-01-08 01:42:17 -0200

    Make it more mobile-friendly

--- header.php
+++ header.php
@@ -5,9 +5,12 @@
     <link rel="alternate" type="application/atom+xml" title="RSS"
           href="https://dev.hackerexperience.com/phame/blog/feed/1/" />
     <?php echo $skin->getCSSResources(); ?>
-
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
   </head>
   <body>
+    <ul id="oblivious-header">
+        <li><a href="<?php echo _e($home_uri); ?>"><?php echo _e($blog->getName()); ?></a></li>
+    </ul>
     <div class="oblivious-info">
       <h1>
         <a href="<?php echo _e($home_uri); ?>"><?php
